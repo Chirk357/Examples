fde5318e05d04799ce23b6a6629c780b7fa8e521
Ekaterina <c.hirko.va@hotmail.com>
2023-02-13 15:03:02 +0300

    example004_division added

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int numberA = 12; 
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB); --> если работаем в целых числах, то ответ будет 2
+// ВМесто int нужно поставить double
+
+
+
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
