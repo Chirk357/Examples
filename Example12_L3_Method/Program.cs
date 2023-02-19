74c4381f405dd38f3cb6dae08f0f1aac78db6e5d
Ekaterina <c.hirko.va@hotmail.com>
2023-02-19 13:08:20 +0300

    xample12_3_ethod

---
+++ Example12_L3_Method/Program.cs
@@ -0,0 +1,30 @@
+﻿// Функции в программировании/
+// Это IV метод.
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+// маленькими “с”.
+
+// string s = "qwerty"
+// s [3] --> будет буква r
+
+string text = "-Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace(string text, char OldValue, char NewValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(text[i] == OldValue) result = result + $"{NewValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
